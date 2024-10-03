8b57726e70d1ee0e5a0dca977283559ffb147e85
wiktormbabyshank <u20_wiktormadry@zsp1.siedlce.pl>
2024-10-03 20:37:52 +0200

    login.php

--- sklep/login.php
+++ sklep/login.php
@@ -1,37 +1,32 @@
 <?php
-session_start(); // Rozpocznij sesję
+session_start(); 
 
-$servername = "localhost"; // Zmień, jeśli masz inny serwer
-$username = "root"; // Zmień, jeśli masz innego użytkownika
-$password = ""; // Zmień, jeśli masz inne hasło
-$dbname = "uzytkownicy_sklepu"; // Zmień na nazwę swojej bazy danych
+$servername = "localhost"; 
+$username = "root"; 
+$password = ""; 
+$dbname = "uzytkownicy_sklepu";
 
-// Tworzenie połączenia
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Sprawdzenie połączenia
 if ($conn->connect_error) {
     die("Połączenie nieudane: " . $conn->connect_error);
 }
 
-// Sprawdzenie, czy formularz został wysłany
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    // Zapytanie do bazy danych
     $sql = "SELECT * FROM uzytkownicy WHERE email = '$email'";
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
-        $user = $result->fetch_assoc(); // Pobierz dane użytkownika
+        $user = $result->fetch_assoc(); 
 
-        // Sprawdzenie hasła
-        if (password_verify($password, $user['haslo'])) { // Użyj password_verify do weryfikacji hasła
-            // Zalogowany pomyślnie
-            $_SESSION['loggedin'] = true; // Ustaw flagę zalogowania
-            $_SESSION['imie'] = $user['imie']; // Ustaw imię
-            $_SESSION['email'] = $user['email']; // Ustaw email
+        if (password_verify($password, $user['haslo'])) { 
+            $_SESSION['loggedin'] = true; 
+            $_SESSION['imie'] = $user['imie']; 
+            $_SESSION['email'] = $user['email']; 
             header("Location: dashboard.php");
             exit();
         } else {
